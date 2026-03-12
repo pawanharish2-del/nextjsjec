d40bff8a2e5e4151ef03577685cf22e9158b3b0d
Deepika-455 <deepikasinghshekhawat13@gmail.com>
2026-03-12 15:35:06 +0530

    pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -111,15 +111,15 @@ export default function RootLayout({ children }) {
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1683144516178369');
-            fbq('track', 'PageView');
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1317160820242336');
+fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
@@ -127,7 +127,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1683144516178369&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1317160820242336&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
